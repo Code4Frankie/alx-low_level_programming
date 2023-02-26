39195b69c4246a40e628dfb086bc857b9222f105
Ahile Shagba Francis <frankiecodesolutions@gmail.com>
2023-02-26 03:09:06 -0800

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabet lower case
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 12)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
